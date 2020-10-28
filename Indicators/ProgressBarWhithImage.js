5f5677ca1d3efb75f3b454669596eae5445392bd
Cristian Vargas <cfelipevargas97@gmail.com>
2020-10-28 15:09:03 -0500

    Add Logic Random Bar

--- Indicators/ProgressBarWhithImage.js
+++ Indicators/ProgressBarWhithImage.js
@@ -5,19 +5,36 @@
 // backgroundColor = default(#fff)
 // styleContainer = {}
 
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import ProgressBar from './ProgressBar'
 import Flex from '../Containers/Flex'
+let flagFun = 'add'
 export default function ProgressBarWhithImage(props) {
+    const [percentage, setPercentage] = useState(0)
+    useEffect(() => {
+        setTimeout(() => {
+            if (flagFun === 'add') {
+                setPercentage(percentage + 1)
+                if (percentage === 100) {
+                    flagFun = 'rest'
+                }
+            } else if (flagFun === 'rest') {
+                setPercentage(percentage - 1)
+                if (percentage === 0) {
+                    flagFun = 'add'
+                }
+            }
+        }, 10);
+    }, [percentage])
     return (
         <Flex>
-            <div style={{position:"relative",height:"100%",left:props.percentage+"%",zIndex:"2"}}>
+            <div style={{position:"relative",height:"100%",left:percentage+"%",zIndex:"2"}}>
                 <img alt=""
                     src={props.imageRelative ? props.imageRelative : ''}
                 >
                 </img>
             </div>
-            <ProgressBar {...props} ></ProgressBar> 
+            <ProgressBar {...props} percentage={percentage}></ProgressBar> 
         </Flex>
     )
 }
